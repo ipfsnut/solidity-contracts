f590d8d928b437aed4efc8607dfe38a0dd7d7fd2
Dylan <40326908+ipfsnut@users.noreply.github.com>
2024-04-24 13:39:52 -0500

    Update Airdrop.sol
    Airdrop.sol is a solidity smart contract you can send your tokens to. Once it has the tokens, you can feed in a list of addresses and airdrop your tokens to them by calling the function on Etherscan.

--- Airdrop.sol
+++ Airdrop.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AirdropContract {
+    ERC20 public token;
+    address[] public recipients;
+
+    constructor() {}
+
+    function airdrop(
+        ERC20 _token,
+        address[] memory _recipients,
+        uint256 _totalAirdropAmount
+    ) external {
+        token = _token;
+        recipients = _recipients;
+
+        require(token.balanceOf(address(this)) >= _totalAirdropAmount, "Insufficient token balance");
+
+        uint256 airdropAmountPerRecipient = _totalAirdropAmount / recipients.length;
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            token.transfer(recipients[i], airdropAmountPerRecipient);
+        }
+    }
+}
 
